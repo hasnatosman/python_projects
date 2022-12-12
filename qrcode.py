6dc9c1986d8017970c34fb2be48acd4bee306aea
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2022-12-12 15:38:24 +0600

    Update qrcode.py

--- qrcode.py
+++ qrcode.py
@@ -1,3 +1,11 @@
-import qrcode as qr
-img = qr.make('Hasnat Osman')
+# import qrcode as qr
+# img = qr.make('Hasnat Osman')
+# img.save('hasnat.png')
+
+import qrcode
+from PIL import Image
+qr = qrcode.QRCode(version = 1,error_correction = qrcode.constants.ERROR_CORRECT_H,box_size = 10, border = 10)
+qr.add_data('https://hasnatosman.wordpress.com/')
+qr.make(fit = True)
+img = qr.make_image(fill_color = 'blue',back_color = 'white')
 img.save('hasnat.png')
